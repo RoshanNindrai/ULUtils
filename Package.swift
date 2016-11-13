4a46e004a33004491759d3eb5e0a31777696551e
Roshan Balaji Nindrai SenthilNatha <RoshanNindrai@Roshans-MacBook-Pro.local>
2016-11-13 10:26:58 -0500

    repo cleanup and removed demo

--- Package.swift
+++ Package.swift
@@ -1,6 +1,6 @@
 import PackageDescription
 
 let package = Package(
-    name: "Utilities"
+    name: "Utils"
 )
 
--- Tests/LinuxMain.swift
+++
@@ -1,6 +0,0 @@
-import XCTest
-@testable import UtilitiesTests
-
-XCTMain([
-     testCase(UtilitiesTests.allTests),
-])
